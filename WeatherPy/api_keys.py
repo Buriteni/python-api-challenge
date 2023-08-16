979a64e757f2d4c797998ac49571c467c8435771
Brittney Hill <britthillre@gmail.com>
2023-08-16 14:48:21 -0500

    Addition

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "b07c92ef004d948125a204978fc01c1a"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "558855f0635d4eccbab3e09249727dc6"
